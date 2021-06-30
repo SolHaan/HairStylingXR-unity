b0a4c4b049a95708ed4efceeba993e0243950435
유상욱 <tkddnr032@gmail.com>
2021-06-30 20:04:32 +0900

    멀티플레이어 플레이어 스폰, 포톤뷰 추가

---
+++ Assets/02.Scripts/Network/NetworkPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkPlayer : MonoBehaviourPunCallbacks
+{
+    public PhotonView PV;
+
+    public float moveSpeed = 10.0f;
+    public float rotateSpeed = 80.0f;
+
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    void Update()
+    {
+        if (PV.IsMine)
+        {
+            float Move = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+            float Rotate = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
+
+            transform.Translate(0, 0, Move);
+            transform.Rotate(0, Rotate, 0);
+        }
+    }
+}
