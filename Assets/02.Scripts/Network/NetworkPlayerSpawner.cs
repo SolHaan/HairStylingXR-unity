b46bd48313bb0ef82bc9118dc405a77b546c6871
유상욱 <tkddnr032@gmail.com>
2021-07-08 14:45:24 +0900

    멀티플레이어 선형보간

--- Assets/02.Scripts/Network/NetworkPlayerSpawner.cs
+++ Assets/02.Scripts/Network/NetworkPlayerSpawner.cs
@@ -7,13 +7,13 @@ using Photon.Realtime;
 public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
     private GameObject spawnedPlayerPrefab;
-    private Transform target;
-    private Vector3 offset;
 
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
-        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Player", transform.position, transform.rotation);
+        Vector3 randPos = Random.insideUnitSphere * 10;
+        randPos.y = 0;
+        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Player", randPos, Quaternion.identity);
     }
 
     public override void OnLeftRoom()
@@ -21,15 +21,4 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
         base.OnLeftRoom();
         PhotonNetwork.Destroy(spawnedPlayerPrefab);
     }
-
-    void Start()
-    {
-        offset = new Vector3(0f, 3f, -8f);
-    }
-
-    void Update()
-    {
-        target = spawnedPlayerPrefab.transform;
-        transform.position = target.position + offset;
-}
 }
