448fc6771161d7123a8e1ea37b2898dc84bf85c8
amozo <80498475+amozo00@users.noreply.github.com>
2021-07-22 12:27:56 +0900

    hair render 환경 구성
    hdrp 추가 및 렌더링 오류 수정

---
+++ Assets/Sol/Scripts/Rotate_Model.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Model : MonoBehaviour
+{
+    //model rotate speed
+    public float speed_ = 5f;
+
+    void Update()
+    {
+        //모델 회전
+        float hori = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.up * speed_ * hori);
+    }
+}
